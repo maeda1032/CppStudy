e3ae0ad1ab37b7ab53a32ea46f8ebdf096ff4267
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-06-13 13:39:16 +0900

    kadai3

--- Cpp/Cpp/Cpp.cpp
+++ Cpp/Cpp/Cpp.cpp
@@ -1,8 +1,31 @@
 ﻿
 #include <iostream>
 
+class MyClass 
+{
+public:
+
+
+	std::weak_ptr<MyClass> ptr_ = {};
+};
+
+
+void func(int)
+{
+	std::cout << "intが呼ばれました。" << std::endl;
+}
+
+void func(char*)
+{
+	std::cout << "charが呼ばれました。" << std::endl;
+}
+
+
 int main()
 {
-   
+	{
+		//NULLを渡した場合、無効なアドレスを持つポインタではなくint型の0と表示されるため
+		func(nullptr);
+	}
 }
 
