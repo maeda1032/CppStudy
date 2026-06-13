92cb3537c3516fac6e9f76b8d955a96377ad15d3
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-06-13 15:00:01 +0900

    途中

--- Cpp/Cpp/Cpp.cpp
+++ Cpp/Cpp/Cpp.cpp
@@ -1,8 +1,39 @@
 ﻿
 #include <iostream>
 
+class Character
+{
+private:
+	char name[10];
+	int hp;
+	int attack;
+
+public:
+
+	void StatusUpdate(int hp1,int attack1)
+	{
+		hp = hp1;
+		attack = attack1;
+	}
+	void PrintStatus(Character chara)
+	{
+		std::cout << chara.name << std::endl;
+		std::cout << chara.hp << std::endl;
+		std::cout << chara.attack << std::endl;
+	}
+};
+
+
+
+
 int main()
 {
-   
+	Character chara1;
+	chara1.StatusUpdate(53,25);
+	chara1.PrintStatus(chara1);
+
+	Character chara2;
+	chara1.StatusUpdate( 532, 255);
+	chara1.PrintStatus(chara2);
 }
 
