ea05e013ef04b65b0a61391661cbb95e0572be00
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-06-13 15:34:19 +0900

    pointer

--- Cpp/Cpp/Cpp.cpp
+++ Cpp/Cpp/Cpp.cpp
@@ -1,8 +1,24 @@
 ﻿
 #include <iostream>
+#include<stdio.h>
+
+void Swap(int* pointInt, int& sansyouValue)
+{
+	int	a = 100;
+	int	b = 500;
+	pointInt = &a;
+	printf("ポインターが呼ばれました。数値は%dです\n",pointInt);
+
+	sansyouValue = b;
+	printf("参照が呼ばれました。数値は%dです\n", sansyouValue);
+}
+
 
 int main()
 {
-   
+	int* pointValue = 0;
+	int sansyouValue = 0;
+	printf("入れ替え前の数値は&d,&dです。", pointValue, sansyouValue);
+	Swap(pointValue, sansyouValue);
 }
 
