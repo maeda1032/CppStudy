58737c5ec26129e12f9109d82d02d62d524becd6
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-06-15 12:11:36 +0900

    作成

--- Cpp/Cpp/Cpp.cpp
+++ Cpp/Cpp/Cpp.cpp
@@ -3,6 +3,28 @@
 
 int main()
 {
-   
+	int array[6];
+	array[0] = 5;
+	array[1] = 15;
+	array[2] = 52;
+	array[3] = 53;
+	array[4] = 354;
+	array[5] = 65;
+
+	int* p = array;
+
+	for (int i = 0; i < 6; i++)
+	{
+		std::cout << array[i] << std::endl;
+	}
+
+
+
+	for (int i = 0; i < 6; i++)
+	{
+		std::cout << *p << std::endl;
+
+		p++;
+	}
 }
 
