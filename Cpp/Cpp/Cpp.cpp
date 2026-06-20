06c2c04bb0a1aefc94e8e35184f5153c171bb32a
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-06-20 18:07:42 +0900

    kansei

--- Cpp/Cpp/Cpp.cpp
+++ Cpp/Cpp/Cpp.cpp
@@ -1,17 +1,26 @@
 ﻿
 #include <iostream>
 
-static int incetance = 0;
 
 class MyClass
 {
 public:
+
+	static int incetance;
+
 	MyClass();
 	~MyClass();
-	static void IncetanceCount(int incetance);
+
+	static void IncetanceCount();
+
+	void functionA() const
+	{
+		std::cout << incetance << std::endl;
+	}
 private:
 
 };
+int MyClass::incetance = 0;
 
 MyClass::MyClass()
 {
@@ -23,13 +32,21 @@ MyClass::~MyClass()
 	incetance -= 1;
 }
 
-void MyClass::IncetanceCount(int incetance)
+void MyClass::IncetanceCount()
 {
 	std::cout << incetance << std::endl;
 }
 
+
 int main()
 {
-   
+	MyClass::incetance = 0;
+
+	MyClass::IncetanceCount();
+	{
+		MyClass myClass;
+		myClass.functionA();
+	}
+	MyClass::IncetanceCount();
 }
 
