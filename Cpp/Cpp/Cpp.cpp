212e1b34dbb7f9c7f818cb7333b6082dfdd1ef10
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-07-09 13:43:35 +0900

    作成

--- Cpp/Cpp/Cpp.cpp
+++ Cpp/Cpp/Cpp.cpp
@@ -1,8 +1,47 @@
 ﻿
 #include <iostream>
 
+class Character
+{
+public:
+	int attack = 0;
+	int hp = 0;
+	int animal = 0;
+
+};
+
+Character* CharacterStatus()
+{
+	Character* chara = new Character;
+
+	chara->animal = 10;
+	chara->hp = 20;
+	chara->attack = 30;
+
+	return chara;
+}
+
+void disp(Character* chara)
+{
+	std::cout << "animalの数" << chara->animal << std::endl;
+	std::cout << "hpの数値" << chara->hp << std::endl;
+	std::cout << "attackの数値" << chara->attack << std::endl;
+}
+
+void DestroyInstance(Character*& chara)
+{
+	if (chara == nullptr) return;
+	delete chara;
+	chara = nullptr;
+	std::cout << "Characterが破棄されました。" << std::endl;
+}
+
 int main()
 {
-   
+	Character* newChara = CharacterStatus();
+
+	disp(newChara);
+
+	DestroyInstance(newChara);
 }
 
