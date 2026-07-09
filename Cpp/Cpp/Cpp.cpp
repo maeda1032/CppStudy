465c6996901b21c17c343c7cb611f82f54545f13
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-07-09 16:28:09 +0900

    作成

--- Cpp/Cpp/Cpp.cpp
+++ Cpp/Cpp/Cpp.cpp
@@ -1,8 +1,27 @@
 ﻿
 #include <iostream>
 
+class UniPtr
+{
+public:
+	UniPtr();
+	~UniPtr();
+};
+
+UniPtr::UniPtr()
+{
+	std::cout << "生成" << std::endl;
+}
+
+UniPtr::~UniPtr()
+{
+	std::cout << "破棄" << std::endl;
+}
+
 int main()
 {
-   
+	{
+		std::unique_ptr<UniPtr> ptr = std::make_unique<UniPtr>();
+	}
 }
 
