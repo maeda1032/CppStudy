b4ec870919180f704f4de0efbd092bee11286524
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-07-09 16:47:20 +0900

    循環参照

--- Cpp/Cpp/Cpp.cpp
+++ Cpp/Cpp/Cpp.cpp
@@ -1,8 +1,40 @@
 ﻿
 #include <iostream>
+#include <memory>
+
+class UniPtr
+{
+public:
+	UniPtr();
+	~UniPtr();
+
+	std::shared_ptr<UniPtr> ptr_ = {};
+};
+
+UniPtr::UniPtr()
+{
+	std::cout << "生成" << std::endl;
+}
+
+UniPtr::~UniPtr()
+{
+	std::cout << "破棄" << std::endl;
+}
 
 int main()
 {
-   
+	{
+
+		{
+			std::cout << "スコープ開始地点" << std::endl;
+			std::shared_ptr<UniPtr> ptr1 = std::make_shared<UniPtr>();
+			std::shared_ptr<UniPtr> ptr2 = std::make_shared<UniPtr>();
+
+			ptr1->ptr_ = ptr2;
+			ptr2->ptr_ = ptr1;
+
+			std::cout << "スコープ終了地点" << std::endl;
+		}
+	}
 }
 
