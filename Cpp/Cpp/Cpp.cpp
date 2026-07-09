97934d8e9ce586928bcb33bd4df214985e6e87a2
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-07-09 17:25:20 +0900

    循環参照回避

--- Cpp/Cpp/Cpp.cpp
+++ Cpp/Cpp/Cpp.cpp
@@ -1,8 +1,41 @@
 ﻿
 #include <iostream>
+#include <memory>
+
+class UniPtr
+{
+public:
+	UniPtr();
+	~UniPtr();
+
+	std::weak_ptr<UniPtr> ptr_ = {};
+	int value = 0;
+};
+
+UniPtr::UniPtr()
+{
+	std::cout << "Instantiate" << std::endl;
+}
+
+UniPtr::~UniPtr()
+{
+	std::cout << "Delete" << std::endl;
+}
 
 int main()
 {
-   
+	{
+
+		{
+			std::cout << "ScopeStartPoint" << std::endl;
+			std::shared_ptr<UniPtr> ptr1 = std::make_shared<UniPtr>();
+			std::shared_ptr<UniPtr> ptr2 = std::make_shared<UniPtr>();
+
+			ptr1->ptr_ = ptr2;
+			ptr2->ptr_ = ptr1;
+
+			std::cout << "ScopeEndPoint" << std::endl;
+		}
+	}
 }
 
