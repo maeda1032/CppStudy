c3ffd39ae0596546b95d390b249a9ed4a9883e99
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-07-18 15:23:23 +0900

    作成

--- Cpp/Cpp/Cpp.cpp
+++ Cpp/Cpp/Cpp.cpp
@@ -1,8 +1,29 @@
 ﻿
 #include <iostream>
+#include <functional>
+#include<vector>
+#include<algorithm>
+
+//2つの値を
+bool greater(int l, int r)
+{
+	return l > r;
+}
 
 int main()
 {
-   
+	//begin最初
+	//end 最後まで
+	//
+	std::vector<float> vec = { 10,8,4,9,2,3,5 };
+
+	//小さい値から大きい値の順に並び変える
+	std::sort(vec.begin(), vec.end()),greater;
+
+	for (std::vector<float>::iterator it = vec.begin(); it != vec.end(); ++it)
+	{
+		std::cout << *it << std::endl;
+	}
 }
+
 
