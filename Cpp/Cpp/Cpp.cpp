2c4aa7bbb4359c6b16c5f51be8d9b11bbb0b18a7
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-07-18 11:26:40 +0900

    sakusei

--- Cpp/Cpp/Cpp.cpp
+++ Cpp/Cpp/Cpp.cpp
@@ -1,8 +1,35 @@
 ﻿
 #include <iostream>
+#include <map>
 
 int main()
 {
-   
+	//ペアで覚えたいとき
+	std::map<char, float >map;
+
+	{
+		map.emplace('a', 3.1f);
+	}
+
+	{
+		float f2 = map['a'];
+	}
+
+	//実装方法
+	{
+		//メンバ関数を使って更新
+		map.at('a') = 0.3f;
+
+		//[]で更新
+		map['a'] = 100.5f;
+	}
+
+	//削除方法
+	{
+		//全部削除
+		map.clear();
+
+		//
+	}
 }
 
