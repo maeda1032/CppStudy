a35dfcd2d1b0d0fbb6b58a9e1fd8085a89b9c6df
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-08-01 15:26:43 +0900

    途中

--- Cpp/Cpp/Cpp.cpp
+++ Cpp/Cpp/Cpp.cpp
@@ -1,8 +1,24 @@
 ﻿
 #include <iostream>
+#include <vector>
 
 int main()
 {
-   
+	int num;
+	std::vector<int> nums = {1,2,3,4,5,6,7,8,9,10};
+
+	for (std::vector<int>::iterator it = nums.begin(); it != nums.end();)
+	{
+
+		it = nums.erase(it);
+	}
+
+	for (std::vector<int>::iterator it = nums.begin(); it != nums.end(); ++it)
+	{
+		int num = *it;
+		std::cout << num << std::endl;
+	}
+
+
 }
 
