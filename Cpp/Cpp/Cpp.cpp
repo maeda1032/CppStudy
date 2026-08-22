88f2b58db1ae2fe4edef58c7f0ccc1a6f0b39d6b
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-08-22 13:20:36 +0900

    sakusei

--- Cpp/Cpp/Cpp.cpp
+++ Cpp/Cpp/Cpp.cpp
@@ -1,8 +1,45 @@
 ﻿
 #include <iostream>
 
-int main()
-{
-   
+class HasDrawA {
+public:
+	void draw() { std::cout << "HasDrawA::draw()が呼び出されました" << std::endl; }
+};
+
+class HasDrawB {
+public:
+	void draw() { std::cout << "HasDrawB::draw()が呼びだれました" << std::endl; }
+};
+
+class NoneDraw{};
+
+template<class T>
+concept HasDrawObject = requires(T & t) {
+	t.draw();
+};
+
+template <class T>
+void callDraw(T& x) {
+	if constexpr (HasDrawObject<T>)
+	{
+		x.draw();
+	}
+	else
+	{
+		std::cout << "draw()が呼び出せません" << std::endl;
+	}
 }
 
+int main()
+{
+	HasDrawA a;
+	HasDrawB b;
+	NoneDraw n;
+	
+	callDraw(a);
+	callDraw(b);
+	callDraw(n);
+
+	return 0;
+	
+}
